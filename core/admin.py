f2ed36f0291b99fcbf0ba522386a9501a8aa124a
“arif-vue” <arif.nuxt@gmail.com>
2026-02-20 16:22:19 +0600

    feat: register models in admin, add .gitignore

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import BrokerAccount, Signal, Order, UserActivityLog
 
-# Register your models here.
+admin.site.register(BrokerAccount)
+admin.site.register(Signal)
+admin.site.register(Order)
+admin.site.register(UserActivityLog)
